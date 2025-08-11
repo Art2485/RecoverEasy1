3236801d595c31a5bd1ff409570b9856f19abbcd
Art2485 <ooartyguitaroo@gmail.com>
2025-08-11 22:48:53 +0700

    Update MainActivity.kt

--- app/MainActivity.kt
+++ app/MainActivity.kt
@@ -1,11 +1,16 @@
-package com.recovereasy.app
+package com.recovereasy
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.recovereasy.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
