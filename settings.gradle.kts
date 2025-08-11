9948b898cc2776a77bad2b416b5214e9b4ca8e22
Art2485 <ooartyguitaroo@gmail.com>
2025-08-11 23:01:20 +0700

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,10 +4,9 @@ pluginManagement {
         mavenCentral()
         gradlePluginPortal()
     }
-    // ระบุ "เวอร์ชัน" ของปลั๊กอินไว้ที่นี่อย่างชัดเจน
     plugins {
         id("com.android.application") version "8.5.2"
-        id("org.jetbrains.kotlin.android") version "1.9.24")
+        id("org.jetbrains.kotlin.android") version "1.9.24"
     }
 }
 
